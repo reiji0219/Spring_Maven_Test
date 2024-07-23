ce3a36795d8dbf834d8119db2fccf423ebd7d36f
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-07-23 20:02:17 +0900

    改行のみ

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
+public class Demo1Application 
+{
+	public static void main( String[] args ) 
+	{
+		SpringApplication.run( Demo1Application.class, args );
 		
 			System.out.println( "終わりだよー" );
 	}
