287625d91013a2ba11c16a39f17f732811037e2e
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-07-24 12:13:02 +0900

    Revert "Controll"
    This reverts commit e73b30cbc81df11df74d6c2b5d7c1504d394ae81.

---
+++ demo-1/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController 
+{
+	@Autowired							//この2行は「postgreSQL」に後でくっつける為に記述
+	JdbcTemplate jdbcTemplate; 
+	
+	@GetMapping("/")					//「Hello」と言う名の「HTMLファイル」を探す為の記述
+	public String index(){				//「http://localhost:8080/」とアドレスバーに記述すると
+		return "Hello" ;					//HTMLファイルの「Hello WebWorld」が表示される
+	}											
+	
+	@PostMapping("name")
+	public String name(@RequestParam( "name" ) String name, Model model ) {
+			List<Map<String,Object>> list;
+			list = jdbcTemplate.queryForList( "select * from users" );
+			model.addAttribute( "users", list );
+			model.addAttribute( "name", name );
+		return "name";
+	}
+	
+}
